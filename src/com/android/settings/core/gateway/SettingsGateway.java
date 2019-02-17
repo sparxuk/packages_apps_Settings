omnirom/android_packages_apps_Settings

[3/3] Settings: keyguard weather view
Change-Id: I4b82872fa1a498802b88ccffc3ed2fd8059545f2

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,8 +140,9 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import org.omnirom.omnigears.OmniDashboardFragment;
 import org.omnirom.omnigears.ButtonSettings;
 import org.omnirom.omnigears.GestureSettings;
-import org.omnirom.omnigears.interfacesettings.BarsSettings;
 import org.omnirom.omnigears.batterylight.BatteryLightSettings;
+import org.omnirom.omnigears.interfacesettings.BarsSettings;
+import org.omnirom.omnigears.interfacesettings.LockscreenItemSettings;
 import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
 import org.omnirom.omnigears.moresettings.OmniJawsSettings;
@@ -280,6 +281,7 @@ public class SettingsGateway {
             BarsSettings.class.getName(),
             BatteryLightSettings.class.getName(),
             OmniJawsSettings.class.getName(),
+            LockscreenItemSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -105,8 +105,9 @@ import java.util.Set;
 
 import org.omnirom.omnigears.OmniDashboardFragment;
 import org.omnirom.omnigears.ButtonSettings;
-import org.omnirom.omnigears.interfacesettings.BarsSettings;
 import org.omnirom.omnigears.batterylight.BatteryLightSettings;
+import org.omnirom.omnigears.interfacesettings.BarsSettings;
+import org.omnirom.omnigears.interfacesettings.LockscreenItemSettings;
 import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
 import org.omnirom.omnigears.moresettings.OmniJawsSettings;
@@ -210,6 +211,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(BarsSettings.class);
         addIndex(BatteryLightSettings.class);
         addIndex(OmniJawsSettings.class);
+        addIndex(LockscreenItemSettings.class);
     }
 
     @Override

sparxuk/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,6 +140,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.gzr.wolvesden.WolvesDen;
 import com.gzr.wolvesden.OmniJawsSettings;
+import com.gzr.wolvesden.interfacesettings.LockscreenItemSettings;
 
 public class SettingsGateway {
 
@@ -268,6 +269,7 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             WolvesDen.class.getName(),
             OmniJawsSettings.class.getName(),
+            LockscreenItemSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -104,6 +104,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.gzr.wolvesden.OmniJawsSettings;
+import com.gzr.wolvesden.interfacesettings.LockscreenItemSettings;
 
 public class SearchIndexableResourcesImpl implements SearchIndexableResources {
 
@@ -196,6 +197,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(PreviouslyConnectedDeviceDashboardFragment.class);
         addIndex(BluetoothDashboardFragment.class);
         addIndex(OmniJawsSettings.class);
+        addIndex(LockscreenItemSettings.class);
     }
 
     @Override