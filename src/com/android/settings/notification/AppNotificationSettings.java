54e003a8a26bac95ef89aa178806292cd7ea0b2c
martinusbe <martinusbe@gmail.com>
2019-08-06 15:50:49 +0200

    AppNotificationSettings.java: fix duplicate onpause()

--- src/com/android/settings/notification/AppNotificationSettings.java
+++ src/com/android/settings/notification/AppNotificationSettings.java
@@ -122,15 +122,6 @@ public class AppNotificationSettings extends NotificationSettingsBase {
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        final Window window = getActivity().getWindow();
-        final WindowManager.LayoutParams attrs = window.getAttributes();
-        attrs.privateFlags &= ~PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS;
-        window.setAttributes(attrs);
-    }
-
-    @Override
     protected String getLogTag() {
         return TAG;
     }
@@ -155,6 +146,10 @@ public class AppNotificationSettings extends NotificationSettingsBase {
     @Override
     public void onPause() {
         super.onPause();
+        final Window window = getActivity().getWindow();
+        final WindowManager.LayoutParams attrs = window.getAttributes();
+        attrs.privateFlags &= ~PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS;
+        window.setAttributes(attrs);
         mNm.forcePulseLedLight(-1, -1, -1);
     }
 
